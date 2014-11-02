321f63411b00bf60fa02d0081aad186c57dfdbc1
Luke Berry <lukeberry99@gmail.com>
2014-11-02 14:16:38 +0000

    Major refactoring (read: starting again)

--- PopcornWrapper.js
+++ PopcornWrapper.js
@@ -1,59 +1,14 @@
 var jsonrpc = require('trac-jsonrpc-client');
-var connected = false;
-var connecting = false;
-var url = "";
-var cli = null;
 
+var connection = null;
 function PopcornWrapper() {
 }
 
-PopcornWrapper.prototype.connect = function(config) {
-    var argCount = 0;
-    for(_obj in config) {
-        if(config.hasOwnProperty(_obj)) {
-            argCount++;
-        }
-    }
-    if(argCount == 4) {
-        // Set URL
-        url = "http://" + config['host'] + ":" + config['port'];
-        // Set CLI
-        cli = new jsonrpc(url, {
-            auth: {  username: config['user'], password: config['pass'] }
-        });
-        // Check connection
-        cli.callRpc('ping', [], function(err, data, result) {
-            if(err) {
-                console.log(err);
-                connected = false;
-            } else {
-                console.log("Connection made");
-                //console.log(data);
-                connected = true;
-                return connected;
-            }
-        });
-    } else {
-        console.error('PopcornWrapper requires config to have 4 arugments, host, port, user and pass');
-    }
-
-}
-
-PopcornWrapper.prototype.sendUp = function(handler) {
-    if(connected) {
-        cli.callRpc('up', [], function(err, data, result) {
-            if(err) {
-                console.error(err);
-            } else { 
-                console.log(data);
-                console.log(result);
-            }
-        });
-        handler(false, result);
-    } else {
-        handler(new Error("Connection not made"));
-    }
-}
-
+PopcornWrapper.prototype.connect = function(cb) {
+    connection = new jsonrpc("http://127.0.0.1:8008", function hasConnected(err, res){ 
+        auth: { username: "popcorn", password: "popcorn" }
+        cb();
+    });
+};
 
 module.exports = PopcornWrapper;
--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-var PopcornWrapper = require('./PopcornWrapper');
+var popcorn = require('./PopcornWrapper');
 
-// Setup your config 
-var config = {
-    host: '127.0.0.1',
-    port: '8008',
-    user: 'popcorn',
-    pass: 'popcorn'
-};
-var popcorn = new PopcornWrapper();
-popcorn.sendUp(function(err, cb) {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log("test");
-    }
+popcorn.connect(function(err, res) {
+    
 });
